5bba54f48810d367c85b011fb35d51548f7f20d7
Sasha <pisarevasas@yandex.ru>
2023-03-15 00:04:26 +0300

    ex

---
+++ exam1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Из имеющего массива выбрать массив, длинна которого не более 3 строк
+
+Console.Clear();
+string[] GetArray = new string[5] {"472", "8", "59370", "hi", "hello"};
+int CountNumber(string[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < GetArray.Length; i++)
+    {
+        if (GetArray[i].Length <= 3)
+        {
+            count++;
+        }
+        
+    }
+    return count;
+}
+
+Console.WriteLine(String.Join(", ", GetArray));
+int count = CountNumber(GetArray);
+Console.WriteLine(count);
